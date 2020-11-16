60a3e79d84fff2890b241ebfc3a806bda0284efe
Sinem Semsioglu <sinem.semsioglu@gmail.com>
2020-11-16 14:06:13 +0300

    implemented test calls to backend in frontend

--- public/index.js
+++ public/index.js
@@ -1,5 +1,36 @@
 const initPage = () => {
+    $('#save-text').click(()=> {
+        sendReq('/saveText',
+            {text: $('#exampleTextInput').val()},
+            () => {
+                    alert('text saved successfully');
+        })
+    })
 
+    $('#generate-text').click(()=> {
+        sendReq('/generateText',
+            {},
+            (data) => {
+                $('#db-info').text(JSON.stringify(data));
+            })
+    })
+}
+
+const sendReq = (path, data, successCallback) => {
+    $.ajax({
+        type: "POST",
+        url: path,
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        success: async (resp) => {
+            if (resp.success) {
+                successCallback(resp.data)
+            } else {
+                alert('problem with request')
+            }
+        },
+        dataType: "json"
+    });
 }
 
 
