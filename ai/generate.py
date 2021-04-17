a6b6f26877d3f4a5e50adba37a830eb8649381c4
burcuozer <73853832+burcuozer@users.noreply.github.com>
2021-04-17 11:19:11 +0300

    generate.py added

---
+++ ai/generate.py
@@ -0,0 +1,36 @@
+import sys
+import os
+import tensorflow as tf
+import json
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+os.environ['TF_XLA_FLAGS'] = '--tf_xla_enable_xla_devices'
+
+dirname = os.path.dirname(__file__)
+req_id = sys.argv[1]
+def main():
+    with open(os.path.join(dirname, 'generated/' + req_id + '.json'), 'w+', encoding='utf-8') as f:
+        resp = dict()
+        one_step_reloaded = tf.saved_model.load(dirname+'/one_step')
+        states = None
+        next_char = tf.constant([sys.argv[2]])
+        result = [next_char]
+
+        for n in range(int(sys.argv[3])):
+              next_char, states = one_step_reloaded.generate_one_step(next_char, states=states)
+              result.append(next_char)
+        #resp["data"] = tf.strings.join(result)[0].numpy().decode("utf-8")
+        #print(tf.strings.join(result)[0].numpy().decode("utf-8"))
+        try:
+            resp["data"] = str(tf.strings.join(result)[0].numpy().decode("utf-8"))
+            print(resp)
+            json.dump(resp, f)
+        except Exception as e:
+               print(str(e))
+        # print(json.dumps(tf.strings.join(result)[0].numpy()))
+
+        f.close()
+main()
+
+
+
